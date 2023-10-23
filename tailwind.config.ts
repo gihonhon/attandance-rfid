b1e94bac28f08e3315921111c9653aa9c076f23e
gihonhon <agunggihon@gmail.com>
2023-10-23 12:17:50 +0800

    UI : Login Form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      maxWidth: {
+        "10xl": "1440px",
+      },
     },
   },
   plugins: [require("daisyui")],
