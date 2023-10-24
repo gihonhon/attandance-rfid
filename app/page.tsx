d5f1b4821cee816d844e0794ba74da81e55313aa
gihonhon <agunggihon@gmail.com>
2023-10-25 00:17:31 +0800

    UI:
    Creating SignUpForm component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,6 @@
 import SignInForm from "@/components/Form/SignInForm";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import SignUpForm from "@/components/Form/SignUpForm";
 
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 
@@ -27,31 +18,13 @@ const Home = () => {
         <Tabs defaultValue="login" className="w-[400px]">
           <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger value="login">Login</TabsTrigger>
-            <TabsTrigger value="password">Register</TabsTrigger>
+            <TabsTrigger value="register">Register</TabsTrigger>
           </TabsList>
           <TabsContent value="login">
             <SignInForm />
           </TabsContent>
-          <TabsContent value="password">
-            <Card>
-              <CardHeader>
-                <CardTitle>Register</CardTitle>
-              </CardHeader>
-              {/** Register */}
-              <CardContent className="space-y-2">
-                <div className="space-y-1">
-                  <Label htmlFor="current">Current password</Label>
-                  <Input id="current" type="password" />
-                </div>
-                <div className="space-y-1">
-                  <Label htmlFor="new">New password</Label>
-                  <Input id="new" type="password" />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button>Save password</Button>
-              </CardFooter>
-            </Card>
+          <TabsContent value="register">
+            <SignUpForm />
           </TabsContent>
         </Tabs>
       </section>
